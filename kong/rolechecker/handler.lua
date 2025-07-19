f9b54594cdf8be0ed1b09d7f101b6a67da69a937
vovantung <vovantungdt123@gmail.com>
2025-07-19 16:56:52 +0700

    cm618

--- kong/rolechecker/handler.lua
+++ kong/rolechecker/handler.lua
@@ -1,27 +1,45 @@
-local BasePlugin = require "kong.plugins.base_plugin"
-local kong = kong
-local RoleChecker = BasePlugin:extend()
+local jwt_decoder = require "kong.plugins.jwt.jwt_parser"
 
-RoleChecker.VERSION  = "1.0.0"
-RoleChecker.PRIORITY = 900
+local RoleChecker = {
+  PRIORITY = 1005,
+  VERSION = "1.0",
+}
 
 function RoleChecker:access(conf)
-  RoleChecker.super.access(self)
+  local auth_header = kong.request.get_header("Authorization")
+  if not auth_header then
+    return kong.response.exit(401, { message = "Missing Authorization header" })
+  end
 
-  local headers = kong.request.get_headers()
-  local role = headers["x-role"]
+  local token = auth_header:match("Bearer%s+(.+)")
+  if not token then
+    return kong.response.exit(401, { message = "Invalid Authorization header" })
+  end
+
+  local jwt, err = jwt_decoder:new(token)
+  if err then
+    return kong.response.exit(401, { message = "Invalid JWT token: " .. err })
+  end
+
+  local claims = jwt.claims
+  local role = claims["role"]
+  if not role then
+    return kong.response.exit(403, { message = "Missing role in token" })
+  end
 
   local path = kong.request.get_path()
 
   if role == "admin" then
-    return -- cho qua hết
-  elseif role == "hrm" then
-    if string.sub(path, 1, 4) == "/hrm" then
+    return
+  elseif role == "user" then
+    if path:find("^/user%-app") then
       return
+    else
+      return kong.response.exit(403, { message = "User role cannot access " .. path })
     end
+  else
+    return kong.response.exit(403, { message = "Unknown role: " .. role })
   end
-
-  return kong.response.exit(403, { message = "Forbidden: insufficient role" })
 end
 
 return RoleChecker
